cc32a0896d2bbf7f95e5b46f674c1a0744084b7c
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-08-11 11:14:16 +0330

    6. start work with graphql

--- app/index.js
+++ app/index.js
@@ -1,12 +1,15 @@
 const express= require('express');
 const http = require('http');
 const mongoose  = require('mongoose');
+const {graphqlHTTP} = require('express-graphql');
+const {buildSchema} = require('graphql');
 const app = express()
 
 module.exports = class Application {
     constructor(){
         this.ServerConfig();
         this.DatabaseConfig();
+        this.Routes();
     }
     ServerConfig(){
         const server = http.createServer(app);
@@ -18,5 +21,23 @@ module.exports = class Application {
     DatabaseConfig(){
         mongoose.Promise = global.Promise;
         mongoose.connect(config.database.url, config.database.options)
+    }
+
+    Routes() {
+        const schema = buildSchema(`
+            type Query{
+                user:String
+            }
+            `);
+        const resolver={
+            user: () => {
+                return "ali"
+            }
+        }
+
+        app.use('/graphql', graphqlHTTP({
+            schema,
+            rootValue:resolver,
+        }))
     }
 }
